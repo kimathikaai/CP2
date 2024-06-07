3302cb149b9b46d98f6eaf4c14be85a27fb1bc09
kimathikaai <kiprotich.kaai@gmail.com>
2024-06-07 15:46:16 -0400

    fixup! fixup! Update script

--- builder.py
+++ builder.py
@@ -478,7 +478,7 @@ class CP2_MOCO(nn.Module):
             # rescale the images
 
             resize = T.Resize(
-                log_grid.shape[1:] * (224 // hidden_image_size[0]),
+                np.array(log_grid.shape[1:]) * (224 // hidden_image_size[0]),
                 T.InterpolationMode.NEAREST_EXACT,
             )
             log_grid = resize(log_grid)
