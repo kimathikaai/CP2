8bc90d2640bd21a1a5bb2322efffa160198d1751
kimathikaai <kiprotich.kaai@gmail.com>
2024-09-11 02:25:26 -0400

    Train with hyper-kvasir

--- scripts/polyp.sh
+++ scripts/polyp.sh
@@ -3,6 +3,7 @@ base_dir_0='/mnt/pub0'
 base_dir_1='/mnt/pub1'
 log_dir="${base_dir_1}/ssl-pretraining/logs"
 data_dir="${base_dir_1}/ssl-pretraining/data"
+pretrain_dir='/mnt/pub1/ssl-pretraining/data/hyper-kvasir/unlabeled-images/images'
 
 # Hyper-parameter
 num_gpus=2
@@ -16,41 +17,41 @@ python -m unittest discover -s tests  -v
 # NO PRE-TRAINING
 #
 
-pretrain_type='NONE'
-for dir in Kvasir-SEG CVC-ClinicDB CVC-ColonDB ETIS-LaribPolypDB
-do
-    for ratio in 0.3 1.0
-    do
-        for seed in 0 1 2
-        do
-            run_id="$(date +"%y%m%d%H%M%S")-${dir}-${pretrain_type}-R${ratio}-S${seed}"
-            current_dir=${data_dir}/${dir}
-            echo "Fine-tuning ${run_id}"
-
-            CUDA_VISIBLE_DEVICES=0,1 python finetune.py \
-                --pretrain_type $pretrain_type \
-                --config $finetune_config_file \
-                --seed $seed\
-                --run_id $run_id\
-                --log_dir $log_dir\
-                --img_dirs $current_dir/Images \
-                --mask_dirs $current_dir/SegmentationImages \
-                --train_data_ratio $ratio \
-                --num_gpus $num_gpus \
-                --num_workers 32 \
-                --batch_size 16 \
-                --img_height 352 \
-                --img_width 352 \
-                --epochs 100
-        done
-    done
-done
+# pretrain_type='NONE'
+# for dir in Kvasir-SEG CVC-ClinicDB CVC-ColonDB ETIS-LaribPolypDB
+# do
+#     for ratio in 0.3 1.0
+#     do
+#         for seed in 0 1 2
+#         do
+#             run_id="$(date +"%y%m%d%H%M%S")-${dir}-${pretrain_type}-R${ratio}-S${seed}"
+#             current_dir=${data_dir}/${dir}
+#             echo "Fine-tuning ${run_id}"
+# 
+#             CUDA_VISIBLE_DEVICES=0,1 python finetune.py \
+#                 --pretrain_type $pretrain_type \
+#                 --config $finetune_config_file \
+#                 --seed $seed\
+#                 --run_id $run_id\
+#                 --log_dir $log_dir\
+#                 --img_dirs $current_dir/Images \
+#                 --mask_dirs $current_dir/SegmentationImages \
+#                 --train_data_ratio $ratio \
+#                 --num_gpus $num_gpus \
+#                 --num_workers 32 \
+#                 --batch_size 16 \
+#                 --img_height 352 \
+#                 --img_width 352 \
+#                 --epochs 100
+#         done
+#     done
+# done
 
 #
 # POLYP PRE-TRAINING
 #
 
-for pretrain_type in CP2 MOCO BYOL PROPOSED
+for pretrain_type in CP2 MOCO BYOL
 do
     #
     # PRE-TRAINING
@@ -66,9 +67,9 @@ do
         --run_id $pretrain_run_id \
         --log_dir $log_dir \
         --pretrain_type $pretrain_type \
-        --data_dirs "$data_dir/CVC-ClinicDB/Images" "$data_dir/CVC-ColonDB/Images" "$data_dir/ETIS-LaribPolypDB/Images" "$data_dir/Kvasir-SEG/Images"   \
+        --data_dirs $pretrain_dir \
         --config $pretrain_config_file \
-        --epochs 200 \
+        --epochs 100 \
         --lr 0.001 \
         --num-workers 64 \
         --batch-size 32 \
@@ -82,9 +83,10 @@ do
     # FINE-TUNE
     #
 
+    # for dir in Kvasir-SEG CVC-ClinicDB CVC-ColonDB ETIS-LaribPolypDB
     for dir in Kvasir-SEG CVC-ClinicDB CVC-ColonDB ETIS-LaribPolypDB
     do
-        for ratio in 0.3 1.0
+        for ratio in 1.0
         do
             for seed in 0 1 2
             do
@@ -113,69 +115,3 @@ do
     done
 done
 
-#
-# IMAGENET PRE-TRAINING
-#
-
-for pretrain_type in CP2 MOCO BYOL PROPOSED
-do
-    #
-    # PRE-TRAINING
-    #
-
-    # Create logging name
-    pretrain_run_id="$(date +"%y%m%d%H%M%S")-pretrain-${pretrain_type}-IMAGENET"
-    echo "Started pre-training for ${pretrain_run_id} IMAGENET"
-
-    # Start pre-training
-    CUDA_VISIBLE_DEVICES=0,1 python main.py \
-        --seed 0 \
-        --run_id $pretrain_run_id \
-        --log_dir $log_dir \
-        --pretrain_type $pretrain_type \
-        --data_dirs "${data_dir}/imagenet1k0_1748/Images" \
-        --config $pretrain_config_file \
-        --epochs 200 \
-        --lr 0.001 \
-        --num-workers 64 \
-        --batch-size 32 \
-        --world-size $num_gpus \
-        --foreground_min 0.5 \
-        --foreground_max 0.8 \
-        --backbone_type 'DEEPLABV3' \
-        --cap_queue
-
-    #
-    # FINE-TUNE
-    #
-
-    for dir in Kvasir-SEG CVC-ClinicDB CVC-ColonDB ETIS-LaribPolypDB
-    do
-        for ratio in 0.3 1.0
-        do
-            for seed in 0 1 2
-            do
-                run_id="$(date +"%y%m%d%H%M%S")-${dir}-${pretrain_type}-R${ratio}-S${seed}-IMAGENET"
-                current_dir=${data_dir}/${dir}
-                echo "Fine-tuning ${run_id}"
-
-                CUDA_VISIBLE_DEVICES=0,1 python finetune.py \
-                    --pretrain_path "${log_dir}/${pretrain_run_id}/checkpoint.ckpt" \
-                    --pretrain_type $pretrain_type \
-                    --config $finetune_config_file \
-                    --seed $seed\
-                    --run_id $run_id\
-                    --log_dir $log_dir\
-                    --img_dirs $current_dir/Images \
-                    --mask_dirs $current_dir/SegmentationImages \
-                    --train_data_ratio $ratio \
-                    --num_gpus $num_gpus \
-                    --num_workers 32 \
-                    --batch_size 16 \
-                    --img_height 352 \
-                    --img_width 352 \
-                    --epochs 100
-            done
-        done
-    done
-done
