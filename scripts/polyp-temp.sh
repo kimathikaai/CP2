eedc70b987e85193d76a76ce55df9b113003766d
kimathikaai <kiprotich.kaai@gmail.com>
2024-09-12 20:47:49 -0400

    fixup! Stop executing after a certain number of steps

--- scripts/polyp-temp.sh
+++ scripts/polyp-temp.sh
@@ -19,24 +19,28 @@ pretrain_type='NONE'
 # PRE-TRAINING
 #
 
-# Create logging name
-pretrain_run_id="$(date +"%y%m%d%H%M%S")-REPRODUCE"
-echo "Started pre-training for ${pretrain_run_id}"
+for i in 0 1
+do
+    # Create logging name
+    pretrain_run_id="$(date +"%y%m%d%H%M%S")-REPRODUCE"
+    echo "Started pre-training for ${pretrain_run_id}"
 
-# Start pre-training
-CUDA_VISIBLE_DEVICES=0,1 python main.py \
-    --seed 0 \
-    --run_id $pretrain_run_id \
-    --log_dir $log_dir \
-    --pretrain_type $pretrain_type \
-    --data_dirs $pretrain_dir \
-    --config $pretrain_config_file \
-    --max_steps 10 \
-    --lr 0.001 \
-    --num-workers 64 \
-    --batch-size 32 \
-    --world-size $num_gpus \
-    --foreground_min 0.5 \
-    --foreground_max 0.8 \
-    --backbone_type 'DEEPLABV3' \
-    --cap_queue
+    # Start pre-training
+    CUDA_VISIBLE_DEVICES=0,1 python main.py \
+        --seed 0 \
+        --run_id $pretrain_run_id \
+        --log_dir $log_dir \
+        --tags $tags \
+        --pretrain_type $pretrain_type \
+        --data_dirs $pretrain_dir \
+        --config $pretrain_config_file \
+        --max_steps 10 \
+        --lr 0.001 \
+        --num-workers 64 \
+        --batch-size 32 \
+        --world-size $num_gpus \
+        --foreground_min 0.5 \
+        --foreground_max 0.8 \
+        --backbone_type 'DEEPLABV3' \
+        --cap_queue
+done
