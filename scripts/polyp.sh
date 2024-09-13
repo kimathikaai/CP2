35d9c87cd059f30b841c4da8d9b730fa8cd26086
kimathikaai <kiprotich.kaai@gmail.com>
2024-09-13 00:19:05 -0400

    Large polyp

--- scripts/polyp.sh
+++ scripts/polyp.sh
@@ -12,46 +12,13 @@ pretrain_config_file='configs/config_pretrain.py'
 
 # Run tests
 python -m unittest discover -s tests  -v
-
-#
-# NO PRE-TRAINING
-#
-
-# pretrain_type='NONE'
-# for dir in Kvasir-SEG CVC-ClinicDB CVC-ColonDB ETIS-LaribPolypDB
-# do
-#     for ratio in 0.3 1.0
-#     do
-#         for seed in 0 1 2
-#         do
-#             run_id="$(date +"%y%m%d%H%M%S")-${dir}-${pretrain_type}-R${ratio}-S${seed}"
-#             current_dir=${data_dir}/${dir}
-#             echo "Fine-tuning ${run_id}"
-# 
-#             CUDA_VISIBLE_DEVICES=0,1 python finetune.py \
-#                 --pretrain_type $pretrain_type \
-#                 --config $finetune_config_file \
-#                 --seed $seed\
-#                 --run_id $run_id\
-#                 --log_dir $log_dir\
-#                 --img_dirs $current_dir/Images \
-#                 --mask_dirs $current_dir/SegmentationImages \
-#                 --train_data_ratio $ratio \
-#                 --num_gpus $num_gpus \
-#                 --num_workers 32 \
-#                 --batch_size 16 \
-#                 --img_height 352 \
-#                 --img_width 352 \
-#                 --epochs 100
-#         done
-#     done
-# done
+tags="$(date +%m-%d-%H%M%S)"
 
 #
 # POLYP PRE-TRAINING
 #
 
-for pretrain_type in CP2 MOCO BYOL
+for pretrain_type in BYOL MOCO CP2
 do
     #
     # PRE-TRAINING
@@ -66,13 +33,14 @@ do
         --seed 0 \
         --run_id $pretrain_run_id \
         --log_dir $log_dir \
+        --tags $tags \
         --pretrain_type $pretrain_type \
         --data_dirs $pretrain_dir \
         --config $pretrain_config_file \
-        --epochs 100 \
+        --epochs 10 \
         --lr 0.001 \
         --num-workers 64 \
-        --batch-size 32 \
+        --batch-size 128 \
         --world-size $num_gpus \
         --foreground_min 0.5 \
         --foreground_max 0.8 \
@@ -86,7 +54,7 @@ do
     # for dir in Kvasir-SEG CVC-ClinicDB CVC-ColonDB ETIS-LaribPolypDB
     for dir in Kvasir-SEG CVC-ClinicDB CVC-ColonDB ETIS-LaribPolypDB
     do
-        for ratio in 1.0
+        for ratio in 0.3 0.6 1
         do
             for seed in 0 1 2
             do
@@ -100,6 +68,7 @@ do
                     --config $finetune_config_file \
                     --seed $seed\
                     --run_id $run_id\
+                    --tags $tags \
                     --log_dir $log_dir\
                     --img_dirs $current_dir/Images \
                     --mask_dirs $current_dir/SegmentationImages \
