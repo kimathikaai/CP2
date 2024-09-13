ccfa3f97a5092a844151d3d80edb613903bc990c
kimathikaai <kiprotich.kaai@gmail.com>
2024-09-12 20:43:52 -0400

    Stop executing after a certain number of steps

--- scripts/polyp-temp.sh
+++ scripts/polyp-temp.sh
@@ -15,35 +15,28 @@ tags="$(date +%m-%d-%H%M%S)-reproducability"
 
 
 pretrain_type='NONE'
-# for dir in Kvasir-SEG CVC-ClinicDB CVC-ColonDB ETIS-LaribPolypDB
-for dir in CVC-ClinicDB
-do
-    for ratio in 1
-    do
-        for seed in 0 0 1 1
-        do
-            run_id="$(date +"%y%m%d%H%M%S")-${dir}-${pretrain_type}-R${ratio}-S${seed}-POLYP"
-            current_dir=${data_dir}/${dir}
-            echo "Fine-tuning ${run_id}"
+#
+# PRE-TRAINING
+#
 
-            CUDA_VISIBLE_DEVICES=0,1 python finetune.py \
-                --pretrain_type $pretrain_type \
-                --config $finetune_config_file \
-                --seed $seed\
-                --run_id $run_id\
-                --tags $tags \
-                --linear_evaluation \
-                --log_dir $log_dir\
-                --img_dirs $current_dir/Images \
-                --mask_dirs $current_dir/SegmentationImages \
-                --train_data_ratio $ratio \
-                --learning_rate 0.0001 \
-                --num_gpus $num_gpus \
-                --num_workers 32 \
-                --batch_size 16 \
-                --img_height 352 \
-                --img_width 352 \
-                --epochs 3
-        done
-    done
-done
+# Create logging name
+pretrain_run_id="$(date +"%y%m%d%H%M%S")-REPRODUCE"
+echo "Started pre-training for ${pretrain_run_id}"
+
+# Start pre-training
+CUDA_VISIBLE_DEVICES=0,1 python main.py \
+    --seed 0 \
+    --run_id $pretrain_run_id \
+    --log_dir $log_dir \
+    --pretrain_type $pretrain_type \
+    --data_dirs $pretrain_dir \
+    --config $pretrain_config_file \
+    --max_steps 10 \
+    --lr 0.001 \
+    --num-workers 64 \
+    --batch-size 32 \
+    --world-size $num_gpus \
+    --foreground_min 0.5 \
+    --foreground_max 0.8 \
+    --backbone_type 'DEEPLABV3' \
+    --cap_queue
