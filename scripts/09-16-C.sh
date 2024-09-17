873ad34931f4518c7f44c32733bdba7d37a59ae4
kimathikaai <kiprotich.kaai@gmail.com>
2024-09-16 21:06:26 -0400

    Evaluate random pretraining

--- scripts/09-16-C.sh
+++ scripts/09-16-C.sh
@@ -18,9 +18,10 @@ tags="$(date +%m-%d-%H%M%S)"
 
 # DESCRIPTION
 # Histopathology 'glas' dataset performance pre-trained on the large SegPath-aSMA dataset
+# Run the none and random variants for the histopathology and polyp datasets
 
-# No pretrain
-for pretrain_type in NONE RANDOM
+# HIST: No pretrain
+for pretrain_type in RANDOM NONE
 do
     for dir in glas
     do
@@ -30,6 +31,41 @@ do
             do
                 run_id="$(date +"%y%m%d%H%M%S")-${dir}-${pretrain_type}-R${ratio}-S${seed}"
                 current_dir=${hist_dir}/${dir}
+                echo "Fine-tuning ${run_id}"
+
+                CUDA_VISIBLE_DEVICES=0,1 python finetune.py \
+                    --pretrain_type $pretrain_type \
+                    --config $finetune_config_file \
+                    --seed $seed\
+                    --run_id $run_id\
+                    --tags $tags \
+                    --log_dir $log_dir\
+                    --img_dirs $current_dir/Images \
+                    --mask_dirs $current_dir/SegmentationImages \
+                    --train_data_ratio $ratio \
+                    --num_gpus $num_gpus \
+                    --num_workers 32 \
+                    --batch_size 16 \
+                    --img_height 352 \
+                    --img_width 352 \
+                    --offline_wandb \
+                    --epochs 100
+            done
+        done
+    done
+done
+
+# POLYP: RANDOM
+for pretrain_type in RANDOM
+do
+    for dir in Kvasir-SEG CVC-ClinicDB CVC-ColonDB ETIS-LaribPolypDB
+    do
+        for ratio in 0.3 0.6 1.0
+        do
+            for seed in 0 1 2
+            do
+                run_id="$(date +"%y%m%d%H%M%S")-${dir}-${pretrain_type}-R${ratio}-S${seed}"
+                current_dir=${data_dir}/${dir}
                 echo "Fine-tuning ${run_id}"
 
                 CUDA_VISIBLE_DEVICES=0,1 python finetune.py \
