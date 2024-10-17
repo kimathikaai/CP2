f1d3f23ba063a5d3a8b0a9c86d752ea511973bc0
kimathikaai <kiprotich.kaai@gmail.com>
2024-10-17 00:29:56 -0400

    Create a script for coord, sym and predictor

--- scripts/10-17-sym-coord.sh
+++ scripts/10-17-sym-coord.sh
@@ -79,6 +79,7 @@ do
         --dense_logits_temp 0.2 \
         --instance_logits_temp 0.2 \
         --use_symmetrical_loss \
+        --use_avgpool_global \
         --cap_queue 
 
     #
@@ -141,6 +142,7 @@ do
         --dense_logits_temp 0.2 \
         --instance_logits_temp 0.2 \
         --use_symmetrical_loss \
+        --use_avgpool_global \
         --cap_queue 
     
     #
@@ -211,6 +213,7 @@ do
         --instance_logits_temp 0.2 \
         --use_symmetrical_loss \
         --use_predictor \
+        --use_avgpool_global \
         --cap_queue 
 
     #
@@ -274,6 +277,140 @@ do
         --instance_logits_temp 0.2 \
         --use_symmetrical_loss \
         --use_predictor \
+        --use_avgpool_global \
+        --cap_queue 
+    
+    #
+    # FINETUNE - POLYP
+    #
+    for dir in Kvasir-SEG CVC-ClinicDB CVC-ColonDB ETIS-LaribPolypDB
+    do
+        for ratio in 0.3 1
+        do
+            for seed in 0 1 2
+            do
+                run_id="$(date +"%y%m%d%H%M%S")-${dir}-${pretrain_type}-R${ratio}-S${seed}-PA"
+                current_dir=${data_dir}/${dir}
+                echo "Fine-tuning ${run_id}"
+
+                CUDA_VISIBLE_DEVICES=2,3 python finetune.py \
+                    --pretrain_path "${log_dir}/${pretrain_run_id}/checkpoint.ckpt" \
+                    --pretrain_type $pretrain_type \
+                    --config $finetune_config_file \
+                    --seed $seed\
+                    --run_id $run_id\
+                    --tags $tags \
+                    --log_dir $log_dir\
+                    --img_dirs $current_dir/Images \
+                    --mask_dirs $current_dir/SegmentationImages \
+                    --train_data_ratio $ratio \
+                    --num_gpus $num_gpus \
+                    --num_workers 32 \
+                    --batch_size 16 \
+                    --img_height 352 \
+                    --img_width 352 \
+                    --epochs 100
+            done
+        done
+    done 
+done
+
+# NO PRED, NO SYM, AVGPOOL, BRANCH 0.5 COORD
+for pretrain_type in PROPOSED_V2
+do
+    #
+    # PRE-TRAINING (Polyp-Small)
+    #
+
+    # Create logging name
+    pretrain_run_id="$(date +"%y%m%d%H%M%S")-pretrain-${pretrain_type}-PA"
+    echo "Started pre-training for ${pretrain_run_id}"
+
+    # Start pre-training
+    CUDA_VISIBLE_DEVICES=2,3 python main.py \
+        --seed 0 \
+        --run_id $pretrain_run_id \
+        --log_dir $log_dir \
+        --tags $tags \
+        --pretrain_type $pretrain_type \
+	    --data_dirs "$data_dir/CVC-ClinicDB/Images" "$data_dir/CVC-ColonDB/Images" "$data_dir/ETIS-LaribPolypDB/Images" "$data_dir/Kvasir-SEG/Images"   \
+        --config $pretrain_config_file \
+        --epochs 200 \
+        --lr 0.001 \
+        --num-workers 64 \
+        --batch-size 32 \
+        --world-size $num_gpus \
+        --foreground_min 0.5 \
+        --foreground_max 0.8 \
+        --backbone_type 'DEEPLABV3' \
+        --lmbd_cp2_dense_loss 0.5 \
+        --dense_logits_temp 0.2 \
+        --instance_logits_temp 0.2 \
+        --lmbd_coordinate 0.5 \
+        --use_avgpool_global \
+        --cap_queue 
+
+    #
+    # FINETUNE - POLYP
+    #
+    for dir in Kvasir-SEG CVC-ClinicDB CVC-ColonDB ETIS-LaribPolypDB
+    do
+        for ratio in 0.3 1
+        do
+            for seed in 0 1 2
+            do
+                run_id="$(date +"%y%m%d%H%M%S")-${dir}-${pretrain_type}-R${ratio}-S${seed}-PA"
+                current_dir=${data_dir}/${dir}
+                echo "Fine-tuning ${run_id}"
+
+                CUDA_VISIBLE_DEVICES=2,3 python finetune.py \
+                    --pretrain_path "${log_dir}/${pretrain_run_id}/checkpoint.ckpt" \
+                    --pretrain_type $pretrain_type \
+                    --config $finetune_config_file \
+                    --seed $seed\
+                    --run_id $run_id\
+                    --tags $tags \
+                    --log_dir $log_dir\
+                    --img_dirs $current_dir/Images \
+                    --mask_dirs $current_dir/SegmentationImages \
+                    --train_data_ratio $ratio \
+                    --num_gpus $num_gpus \
+                    --num_workers 32 \
+                    --batch_size 16 \
+                    --img_height 352 \
+                    --img_width 352 \
+                    --epochs 100
+            done
+        done
+    done
+    
+    #
+    # PRE-TRAIN (HyperK) # baseline
+    #
+    pretrain_run_id="$(date +"%y%m%d%H%M%S")-pretrain-${pretrain_type}-PH"
+    echo "Started pre-training for ${pretrain_run_id}"
+    # Start pre-training
+    CUDA_VISIBLE_DEVICES=2,3 python main.py \
+        --seed 0 \
+        --run_id $pretrain_run_id \
+        --log_dir $log_dir \
+        --tags $tags \
+        --pretrain_type $pretrain_type \
+        --data_dirs $poly_pretrain_dir \
+        --config $pretrain_config_file \
+        --epochs 10 \
+        --lr 0.001 \
+        --num-workers 64 \
+        --batch-size 128 \
+        --world-size $num_gpus \
+        --foreground_min 0.5 \
+        --foreground_max 0.8 \
+        --backbone_type 'DEEPLABV3' \
+        --lmbd_cp2_dense_loss 0.5 \
+        --dense_logits_temp 0.2 \
+        --instance_logits_temp 0.2 \
+        --lmbd_coordinate 0.5 \
+        --use_avgpool_global \
         --cap_queue 
     
     #
